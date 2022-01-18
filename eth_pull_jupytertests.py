85c470d9daff733433b2a7f2085c0b99a6e93333
mattromano <mattromano88@gmail.com>
2022-01-17 19:03:05 -0800

    pulled_harmony_only_contracts

--- eth_pull_jupytertests.py
+++ eth_pull_jupytertests.py
@@ -3,46 +3,90 @@ import pandas as pd
 import time
 import requests
 
-df1 = pd.read_csv("token_list.csv")
-
-print(df1.head())
-contract_address_list = []
-
-for x in df1['TOKEN_ADDRESS']:
-    contract_address_list.append(x)
-
-
-
-#print(contract_address_list)
-
-api_prefix =  "https://api.coingecko.com/api/v3/simple/token_price/harmony-shard-0?contract_addresses=0x72cb10c6bfa5624dd07ef608027e366bd690048f"
-api_suffix = "&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true"
-#for y in contract_address_list:
-#    api_prefix = api_prefix + "%2C" + y
-
-comma_sep_string = ","
-
-for y in contract_address_list:
-    comma_sep_string = comma_sep_string +  y + ","
-
-#print(comma_sep_string) 
-
-coin_list = "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
-
-
-request_url = api_prefix + api_suffix
 request_url = "https://api.coingecko.com/api/v3/simple/token_price/harmony-shard-0?contract_addresses=0x72cb10c6bfa5624dd07ef608027e366bd690048f%2C0x0dc78c79b4eb080ead5c1d16559225a46b580694%2C0x8750f5651af49950b5419928fecefca7c82141e3&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true"
 
+coin_list = "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
 #print(request_url)
 
 response = requests.get(coin_list)
 response = response.json()
 df2 = pd.json_normalize(response)
-#df2["platforms"]
-df2.to_csv("coinlist.csv")
-print(df2.head())
-#print(df2["platforms"])
-#print(response)
+df2.head()
 
-#eth_market_data = eth_price['market_data']['price_change_percentage_1h_in_currency']['usd']
-#print(type(eth_market_data))
+df3 = df2.dropna(subset = ["platforms.harmony-shard-0"])
+
+df4 = df3.drop(
+    [
+ 'platforms.polygon-pos',
+ 'platforms.avalanche',
+ 'platforms.binance-smart-chain',
+ 'platforms.fantom',
+ 'platforms.xdai',
+ 'platforms.kardiachain',
+ 'platforms.moonriver',
+ 'platforms.tron',
+ 'platforms.huobi-token',
+ 'platforms.sora',
+ 'platforms.polkadot',
+ 'platforms.chiliz',
+ 'platforms.komodo',
+ 'platforms.cardano',
+ 'platforms.optimistic-ethereum',
+ 'platforms.ardor',
+ 'platforms.qtum',
+ 'platforms.stellar',
+ 'platforms.arbitrum-one',
+ 'platforms.cronos',
+ 'platforms.solana',
+ 'platforms.osmosis',
+ 'platforms.algorand',
+ 'platforms.celo',
+ 'platforms.aurora',
+ 'platforms.eos',
+ 'platforms.neo',
+ 'platforms.terra',
+ 'platforms.Bitcichain',
+ 'platforms.waves',
+ 'platforms.okex-chain',
+ 'platforms.',
+ 'platforms.ronin',
+ 'platforms.icon',
+ 'platforms.smartbch',
+ 'platforms.nem',
+ 'platforms.bitshares',
+ 'platforms.binancecoin',
+ 'platforms.iotex',
+ 'platforms.fuse',
+ 'platforms.kucoin-community-chain',
+ 'platforms.hoo',
+ 'platforms.zilliqa',
+ 'platforms.klay-token',
+ 'platforms.boba',
+ 'platforms.secret',
+ 'platforms.tezos',
+ 'platforms.fusion-network',
+ 'platforms.xrp',
+ 'platforms.cosmos',
+ 'platforms.telos',
+ 'platforms.gochain',
+ 'platforms.vechain',
+ 'platforms.bitcoin-cash',
+ 'platforms.tomochain',
+ 'platforms.nuls',
+ 'platforms.metis-andromeda',
+ 'platforms.elrond',
+ 'platforms.stratis',
+ 'platforms.kava',
+ 'platforms.kusama',
+ 'platforms.omni',
+ 'platforms.metaverse-etp',
+ 'platforms.nxt',
+ 'platforms.enq-enecuum',
+ 'platforms.ontology',
+ 'platforms.factom',
+ 'platforms.wanchain',
+ 'platforms.rootstock',
+ 'platforms.openledger',
+ 'platforms.vite']
+ ,axis=1
+)
